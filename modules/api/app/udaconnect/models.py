acc363f46d24646d67ef31e85060c38d1f9b8f21
Ben Kelly <Benjamin.J.Kelly@live.com>
2023-05-30 18:05:56 -0400

    Remove Person from API monolith

--- modules/api/app/udaconnect/models.py
+++ modules/api/app/udaconnect/models.py
@@ -12,20 +12,11 @@ from sqlalchemy.dialects.postgresql import JSONB, UUID
 from sqlalchemy.ext.hybrid import hybrid_property
 
 
-class Person(db.Model):
-    __tablename__ = "person"
-
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
-    company_name = Column(String, nullable=False)
-
-
 class Location(db.Model):
     __tablename__ = "location"
 
     id = Column(BigInteger, primary_key=True)
-    person_id = Column(Integer, ForeignKey(Person.id), nullable=False)
+    person_id = Column(Integer, nullable=False)
     coordinate = Column(Geometry("POINT"), nullable=False)
     creation_time = Column(DateTime, nullable=False, default=datetime.utcnow)
     _wkt_shape: str = None
@@ -61,4 +52,4 @@ class Location(db.Model):
 @dataclass
 class Connection:
     location: Location
-    person: Person
+    #person: Person
